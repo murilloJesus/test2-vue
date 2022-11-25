5f14791d0a10973869c96ca20eaa0e1b5baa76bd
Murillo Jesus <murillos.jesus@hotmail.com>
2022-11-25 16:51:56 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test2-vue',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
